c9a9983f5c128d4c2c410a8663942bb37f1a24c4
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2024-10-11 20:04:07 +0200

    6looseChange refactored

--- Arrays & Lists/6looseChange.py
+++ Arrays & Lists/6looseChange.py
@@ -7,14 +7,14 @@ def loose_change(cents):
     
     while cents > 0:
         if cents >= 25:
-            change_dict['Quarters'] += 1
-            cents += -25
+            change_dict['Quarters'] = cents // 25
+            cents = cents%25
         elif cents >= 10:
-            change_dict['Dimes'] += 1
-            cents += -10
+            change_dict['Dimes'] = cents // 10
+            cents = cents%10
         elif cents >= 5:
-            change_dict['Nickels'] += 1
-            cents += -5
+            change_dict['Nickels'] = cents//5
+            cents = cents%5
         elif cents >= 1:
             change_dict['Pennies'] += 1
             cents += -1
