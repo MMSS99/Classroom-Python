fc86dfee804e3b8c4a1d950f448d4be243a1517e
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2024-11-04 15:07:05 +0100

    deleted sudoku -> moved to own repo, added tryexcept notebook

--- zOther(No Codewars)/SUDOKU/RECURSOS/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append('.')
-sys.path.append('..')
-
-from .columnaCorrecta import compColumnas
-from .lineaCorrecta import compLineas
-from .numerosRango import compNumeros
-from .sudokuEstandar import compEstandar
