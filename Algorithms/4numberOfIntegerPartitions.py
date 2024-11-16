08c1b44c5a95ef58f0d78f26d81b84f51f3443ba
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2024-11-16 14:18:45 +0100

    docs(codewars): :memo: 4numberOfInteger algorithm solves other exercise, added as commentary

--- Algorithms/4numberOfIntegerPartitions.py
+++ Algorithms/4numberOfIntegerPartitions.py
@@ -1,5 +1,6 @@
-#https://www.codewars.com/kata/546d5028ddbcbd4b8d001254/
-# Secuencia relevante para solución: https://oeis.org/A000041
+# https://www.codewars.com/kata/546d5028ddbcbd4b8d001254/
+# El algoritmo también resuelve este kata: https://www.codewars.com/kata/52ec24228a515e620b0005ef
+# Secuencia relevante para solución (matriz inversa): https://oeis.org/A000041
 
 def partitions(number):
     sequence = [1] + [0]*number
@@ -7,7 +8,7 @@ def partitions(number):
     for indexBase in range(1,number+1):
         for indexOperation in range(indexBase,number+1):
             sequence[indexOperation] += sequence[indexOperation-indexBase]
-            
+
     return sequence[-1]
 
 if __name__ == "__main__":
