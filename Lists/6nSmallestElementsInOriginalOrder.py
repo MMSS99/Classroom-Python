274f6ec18e657330bebc5d8f3e3ec8b59d18d335
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2024-11-20 11:29:37 +0100

    feat(codewars): :recycle: 6nSmallest refactor initialized

--- Lists/6nSmallestElementsInOriginalOrder.py
+++ Lists/6nSmallestElementsInOriginalOrder.py
@@ -1,7 +1,26 @@
 #https://www.codewars.com/kata/5aec1ed7de4c7f3517000079
 
-def first_n_smallest(arr, n):
+'''def first_n_smallest(arr, n):
     arr = arr[::-1]
     while(len(arr) > n):
         arr.remove(max(arr))
-    return arr[::-1]
+    return arr[::-1]'''
+
+def first_n_smallest(arr, n):
+    print (list(enumerate(arr)))
+    lambda arr, n: while len(arr) > n
+    return [",".join(x for x in )]
+
+
+
+if __name__ == "__main__":
+    assert(first_n_smallest([1,2,3,4,5],3)) == [1,2,3]
+    assert(first_n_smallest([5,4,3,2,1],3)) == [3,2,1]
+    assert(first_n_smallest([1,2,3,1,2],3)) == [1,2,1]
+    assert(first_n_smallest([1,2,3,-4,0],3)) == [1,-4,0]
+    assert(first_n_smallest([1,2,3,4,5],0)) == []
+    assert(first_n_smallest([1,2,3,4,5],5)) == [1,2,3,4,5]
+    assert(first_n_smallest([1,2,3,4,2],4)) == [1,2,3,2]
+    assert(first_n_smallest([2,1,2,3,4,2],2)) == [2,1]
+    assert(first_n_smallest([2,1,2,3,4,2],3)) == [2,1,2]
+    assert(first_n_smallest([2,1,2,3,4,2],4)) == [2,1,2,2]
